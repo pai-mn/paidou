6e927185ed72e52dbebd47918532dfefd23bd056
cbdyzj <cbdyzj@gmail.com>
2026-07-19 08:56:35 +0800

    feat: add Handle to Wordle variants

--- src/web/components/VariantLinks.vue
+++ src/web/components/VariantLinks.vue
@@ -8,6 +8,7 @@ import { t } from '#/web/i18n.ts'
       <b>{{ t('other-variants') }}</b>
     </p>
     <ul class="flex flex-col gap-3 text-left w-50 m-2 list-disc">
+      <li><a href="https://handle.antfu.me/" target="_blank">汉兜 Handle</a> - by Anthony Fu</li>
       <li><a href="https://pinyincaichengyu.com" target="_blank">拼音猜成语</a> - by limboy</li>
       <li><a href="https://cheeaun.github.io/chengyu-wordle/" target="_blank">成语 Wordle</a> - by Chee Aun</li>
       <li><a href="https://apps.apple.com/cn/app/id1606194420" target="_blank">猜文字 (iOS)</a> - by Jinke Du</li>
